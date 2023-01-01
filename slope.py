4255d66db279c9f70a877108ab854e59d34e0669
CleverCaptain <ms.shiva2019@gmail.com>
2023-01-01 13:51:32 +0530

    Continuation

---
+++ slope.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+
+Window.size = (310, 580)
+
+class Slope(MDApp):
+    def build(self):
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(Builder.load_file("main.kv"))
+        screen_manager.add_widget(Builder.load_file("signup.kv"))
+        screen_manager.add_widget(Builder.load_file("login.kv"))
+        return screen_manager
+
+
+LabelBase.register(name="MPoppins", fn_regular="Black.ttf")
+LabelBase.register(name="BPoppins", fn_regular="Italic.ttf")
+Slope().run()
