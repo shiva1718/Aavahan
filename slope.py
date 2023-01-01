5cc6c10b702927fff07f1a212a90912ec3a5174a
CleverCaptain <ms.shiva2019@gmail.com>
2023-01-01 23:40:23 +0530

    added import android.permission

--- slope.py
+++ slope.py
@@ -2,19 +2,39 @@ from kivy.app import App
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
-from android.permissions import request_permissions, Permission
+from kivy.utils import platform
+
+if platform == "android":
+    from android.permissions import request_permissions, Permission
+    request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+else:
+    print(platform)
 
 Window.size = (310, 580)
+
 
 class Slope(MDApp):
     def build(self):
         screen_manager = ScreenManager()
-        screen_manager.add_widget(Builder.load_file("main.kv"))
+        # screen_manager.add_widget(Builder.load_file("main.kv"))
         screen_manager.add_widget(Builder.load_file("signup.kv"))
         screen_manager.add_widget(Builder.load_file("login.kv"))
+        screen_manager.add_widget(Builder.load_file("map.py"))
         return screen_manager
+
+    def request_gps_permission(self, instance):
+        # Request the GPS permission
+        request_permission(Permission.LOCATION, self.grant_permission)
+
+    def grant_permission(self, permission, granted):
+        # If the permission is granted, show a message
+        if granted:
+            print("Permission granted")
+        else:
+            print("Permission denied")
 
 
 LabelBase.register(name="MPoppins", fn_regular="Black.ttf")
